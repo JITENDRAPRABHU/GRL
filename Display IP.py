0e32d68fe9a2f495f815aa048756a83daff860a1
Jitendra prabhu <55188940+JITENDRAPRABHU@users.noreply.github.com>
2021-07-21 16:06:29 +0530

    Display_IP Address

--- Display IP.py
+++ Display IP.py
@@ -1,6 +1,27 @@
 #importing module 
 import socket
-#getting host name and storing it in a variable
-Host_name=socket.gethostname()
-#getting the host Ip address by passing hostname in gethostbyname() method and displaying ip address
-print ("Ip Address:",socket.gethostbyname(Host_name))
+import os
+from tkinter import * 
+from tkinter import messagebox
+
+#function to run ipconfig cmd
+def mycmd():
+    os.system('cmd /c "ipconfig"')
+
+#function to get ip_address
+def get_ip():
+    Host_name=socket.gethostname()
+    ip_add=socket.gethostbyname(Host_name)
+    return ip_add
+
+#function to display ip_address as popup msg
+def popup(ip_add):
+    root = Tk()
+    root.withdraw()
+    messagebox.showinfo("IP ADDRESS: ", ip_add)
+
+if __name__=="__main__":
+    global ip_add
+    mycmd()
+    ip_add=get_ip()
+    popup(ip_add)
